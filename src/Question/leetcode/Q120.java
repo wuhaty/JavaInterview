7fd1df1f40349c5a8a6dbb6e62ecfb1826452cd8
wuhaty <wuhaty@163.com>
2017-04-13 20:18:08 +0800

    fix solution

---
+++ src/Question/leetcode/Q120.java
@@ -0,0 +1,7 @@
+package Question.leetcode;
+
+/**
+ * Created by gump on 2017/4/12.
+ */
+public class Q120 {
+}
---
+++ src/Question/leetcode/Q95.java
@@ -0,0 +1,7 @@
+package Question.leetcode;
+
+/**
+ * Created by gump on 2017/4/10.
+ */
+public class Q95 {
+}
