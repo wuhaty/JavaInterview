cf446d7a46c1383cc763d1ae930bc449d9e70a7c
wuhaty <wuhaty@163.com>
2017-04-13 20:19:22 +0800

    add code

--- src/Question/leetcode/Q120.java
+++ src/Question/leetcode/Q120.java
@@ -1,7 +1,45 @@
 package Question.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gump on 2017/4/12.
  */
 public class Q120 {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        Integer result[] = new Integer[triangle.size()];
+        result = triangle.get(triangle.size()-1).toArray(result);
+
+        for (int i = triangle.size()-2; i >=0 ; i--) {
+            for (int j = 0; j <=i ; j++) {
+                result[j] = Math.min(result[j],result[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return result[0];
+    }
+
+    public static void main(String argc[]){
+        List<List<Integer>> test= new ArrayList();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(3);
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(4);
+        list3.add(5);
+        list3.add(6);
+        List<Integer> list4 = new ArrayList<>();
+        list4.add(7);
+        list4.add(8);
+        list4.add(9);
+        list4.add(10);
+
+        test.add(list1);
+        test.add(list2);
+        test.add(list3);
+        test.add(list4);
+        System.out.println(minimumTotal(test));
+    }
 }
--- src/Question/leetcode/Q95.java
+++ src/Question/leetcode/Q95.java
@@ -1,7 +1,80 @@
 package Question.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gump on 2017/4/10.
  */
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
 public class Q95 {
+
+
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0) return null;
+
+        List<TreeNode>[] result = new List[n+1];
+        result[0] = new ArrayList<TreeNode>();
+        result[0].add(null) ;
+        List<TreeNode> init=new ArrayList<>();
+        init.add(new TreeNode(1));
+        result[1] = init;
+
+        for(int i=2;i<n+1;i++){
+            result[i] =new ArrayList<>();
+            for(int j=1;j<=i;j++){
+                int left = j-1;
+                int right = i-j;
+                List<TreeNode> leftNode = result[left];
+                List<TreeNode> rightNode = result[right];
+
+                if(leftNode!=null && rightNode!=null){
+                    for (TreeNode ln:leftNode) {
+                        for (TreeNode rn:rightNode) {
+                            TreeNode root = new TreeNode(j);
+                            root.left = ln;
+                            root.right = clone(rn,i-right);
+                            result[i].add(root);
+                        }
+                    }
+                }else if(leftNode!=null){
+                    for (TreeNode ln:leftNode) {
+                        TreeNode root = new TreeNode(j);
+                        root.left = ln;
+                        result[i].add(root);
+                    }
+                }else if(rightNode!=null){
+                    for (TreeNode rn:rightNode) {
+                        TreeNode root = new TreeNode(j);
+                        root.right = clone(rn,i-right);
+                        result[i].add(root);
+                    }
+                }
+
+
+
+            }
+        }
+        return result[n];
+    }
+
+    private static TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+
+    public static void main(String argc[]){
+        Q95 q = new Q95();
+        System.out.println(q.generateTrees(2));
+    }
 }
