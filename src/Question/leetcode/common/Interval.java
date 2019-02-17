9450488b751ac95a8dab23b87ce5954f9656124b
gump <wuhaty@163.com>
2019-02-17 15:31:16 +0800

    Use common dependency

---
+++ src/Question/leetcode/common/Interval.java
@@ -0,0 +1,11 @@
+package Question.leetcode.common;
+
+/**
+ * Created by gump on 2019/2/17.
+ */
+public class Interval {
+    public int start;
+    public int end;
+    public Interval() { start = 0; end = 0; }
+    public Interval(int s, int e) { start = s; end = e; }
+}
