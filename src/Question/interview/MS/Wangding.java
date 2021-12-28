1aeffe603be5bc9b750451ec02c01a6d4c281cdb
谭晓楠 <wuhaty@163.com>
2021-12-28 21:42:54 +0800

    wangding test

---
+++ src/Question/interview/MS/Wangding.java
@@ -0,0 +1,64 @@
+package Question.interview.MS;
+
+public class Wangding {
+
+
+    class ListNode {
+        Integer val;
+        ListNode next;
+
+        ListNode(Integer val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 两个单向链表判断是否相交
+     *
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+
+    // what if list node contains ring?
+    private boolean check(ListNode a, ListNode b) {
+        if (a == null || b == null) return false;
+
+        ListNode bigger = a;
+        ListNode smaller = b;
+
+        int aLength = 0, bLength = 0;
+        ListNode pa = a, pb = b;
+
+        while (pa != null) {
+            aLength++;
+            pa = pa.next;
+        }
+        while (pb != null) {
+            bLength++;
+            pb = pb.next;
+        }
+
+        if (bLength > aLength) {
+            bigger = b;
+            smaller = a;
+        }
+
+        int diff = Math.abs(aLength - bLength);
+
+        while (diff > 0) {
+            bigger = bigger.next;
+            diff--;
+        }
+
+        while (bigger != null && smaller != null) {
+            if (bigger == smaller) return true;
+            bigger = bigger.next;
+            smaller = smaller.next;
+        }
+        return false;
+    }
+}
